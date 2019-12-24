865a0c7aa9b59dd1f9dc06c94c859591e6863714
tldrma <6267370+tldrma@users.noreply.github.com>
2019-12-24 15:45:56 +0100

    quick test

--- src/Example.jl
+++ src/Example.jl
@@ -1,18 +1,21 @@
 module Example
-export hello, domath
+export hello, domath, new_fn
 
 """
     hello(who::String)
 
 Return "Hello, `who`".
+
 """
-hello(who::String) = "Hello, $who"
+hello(who::String) = "Hello there, $who"
 
 """
     domath(x::Number)
 
 Return `x + 5`.
 """
+new_fn(x::Float64) = 2*x
 domath(x::Number) = x + 5
+
 
 end
